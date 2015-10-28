07a3f32a78a7a8e9b4a5a1cf4473abf74099785c
eder <edpefal@gmail.com>
2015-10-27 18:55:41 -0600

    not in use

--- app/src/main/java/com/wezen/madison/summary/SummaryActivity.java
+++ app/src/main/java/com/wezen/madison/summary/SummaryActivity.java
@@ -76,27 +76,6 @@ public class SummaryActivity extends DialogActivity implements  OrderDialogFragm
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_summary, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     View.OnClickListener fabListener = new View.OnClickListener() {
         @Override
