6cb567bea7d70ebbd001f8fc7f3a051badb528a6
eder <edpefal@gmail.com>
2016-02-29 17:26:43 -0600

    change password

--- app/src/main/java/com/wezen/madison/password/PasswordActivity.java
+++ app/src/main/java/com/wezen/madison/password/PasswordActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.wezen.madison.R;
 
 public class PasswordActivity extends AppCompatActivity {
@@ -31,44 +32,65 @@ public class PasswordActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateLocalFields();
+                if(validateLocalFields()){
+                    validateInBackground();
+                }
             }
         });
     }
 
 
     private boolean validateLocalFields(){
-        if(TextUtils.isEmpty(editTextNewPassword.getText().toString())){
-            editTextNewPassword.setError(getResources().getString(R.string.error_field_required));
+        if(TextUtils.isEmpty(editTextOldPassword.getText().toString())){
+            editTextOldPassword.requestFocus();
+            editTextOldPassword.setError(getResources().getString(R.string.error_field_required));
             return false;
         }
-        if(TextUtils.isEmpty(editTextOldPassword.getText().toString())){
+        if(TextUtils.isEmpty(editTextNewPassword.getText().toString())){
+            editTextNewPassword.requestFocus();
             editTextNewPassword.setError(getResources().getString(R.string.error_field_required));
             return false;
         }
         if(TextUtils.isEmpty(editTextRepeatPassword.getText().toString())){
-            editTextNewPassword.setError(getResources().getString(R.string.error_field_required));
+            editTextRepeatPassword.requestFocus();
+            editTextRepeatPassword.setError(getResources().getString(R.string.error_field_required));
             return false;
         }
         if(!editTextNewPassword.getText().toString().equals(editTextRepeatPassword.getText().toString())){
-            editTextRepeatPassword.setText(getResources().getString(R.string.passwords_must_been_the_same));
+            editTextRepeatPassword.requestFocus();
+            editTextRepeatPassword.setError(getResources().getString(R.string.passwords_must_been_the_same));
             return false;
         }
+        if(editTextNewPassword.getText().toString().equals(editTextOldPassword.getText().toString())){
+            editTextNewPassword.requestFocus();
+            editTextNewPassword.setError(getResources().getString(R.string.you_can_not_use_the_same_password));
+        }
+
+
+
         return true;
     }
 
 
     private void validateInBackground(){
+
         boolean isValid = false;
-        ParseUser.logInInBackground(ParseUser.getCurrentUser().getUsername(), editTextOldPassword.getText().toString(), new LogInCallback() {
-            @Override
-            public void done(ParseUser parseUser, ParseException e) {
-                if(e== null){
-                    //old passweord is correct
+        ParseUser.logInInBackground(ParseUser.getCurrentUser().getUsername(), editTextOldPassword.getText().toString(), (parseUser, e) -> {
+            if(e== null){
+                //old password is correct
+                parseUser.setPassword(editTextNewPassword.getText().toString());
+                parseUser.saveInBackground(e1 -> {
+                    if(e1 == null){
+                        ParseUser.getCurrentUser().fetchInBackground();
+                    } else {//ups
 
-                } else{//ups
+                    }
 
-                }
+                });
+
+
+            } else{//ups
+
             }
         });
 
