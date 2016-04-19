935eaf0d066010c4a067c686846f65d7b42d05de
eder <edpefal@gmail.com>
2016-04-19 13:47:50 -0500

    user

---
+++ app/src/main/java/com/wezen/madison/user/User.java
@@ -0,0 +1,52 @@
+package com.wezen.madison.user;
+
+/**
+ * Created by eder on 18/02/2016.
+ */
+public class User {
+    private String username;
+    private String createdAt;
+    private String updatedAt;
+    private String objectId;
+    private String sessionToken;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getSessionToken() {
+        return sessionToken;
+    }
+
+    public void setSessionToken(String sessionToken) {
+        this.sessionToken = sessionToken;
+    }
+}
