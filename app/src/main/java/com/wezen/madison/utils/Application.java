7cebfc5ba48320ae3037c7e9cab44471150803f1
eder <edpefal@gmail.com>
2016-04-25 11:43:53 -0500

    missing file

--- app/src/main/java/com/wezen/madison/utils/Application.java
+++ app/src/main/java/com/wezen/madison/utils/Application.java
@@ -1,16 +1,20 @@
 package com.wezen.madison.utils;
 
 import com.parse.Parse;
+import com.parse.ParsePush;
 
 /**
  * Created by eder on 5/25/15.
  */
 public class Application extends android.app.Application {
 
+    public ToolbarColorSinleton toolbarColorSinleton;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "","");
+        Parse.initialize(this, "xJzgkwGEHuFFIBazGyGWeHmbn9cSWaQ2F9oPHFhb", "dSdG1ACby08fUT6oCxxxZxWyXq5sTzm3zW51YNNS");
+        ParsePush.subscribeInBackground("Client");
     }
 }
